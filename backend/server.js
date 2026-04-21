0096383f978125aaa8685431b195a7cdb95390b0
vighnaraj107 <vighnarajkakade@gmail.com>
2026-04-22 00:51:39 +0530

    changed region

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY,
-  region: "ap-south-1",
+  region: "eu-north-1",
 });
 const s3 = new AWS.S3();
 
